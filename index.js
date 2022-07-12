baf791f4748986629839ea533c68a72a970f2af2
Dolhov Andrey <andrej.dolgov.042110@gmail.com>
2022-07-12 19:41:06 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+// 'use strict'
+// // debugger
+// const seq = [3, 9, 1, 11, 13, 5];
+
+
+// seq.sort(function(a, b) {
+//     return a - b;
+// })
+
+// let sqrt = seq.map((i) => {
+//     console.log()
+// })
+
+// console.table(sqrt)
+// // console.log(seq)
+
+// // let result = 0;
+// // let diff = 0;
+
+// // seq.forEach(i => console.log(i.));
+
+// // console.log(diff)
+
+// // for (let i = 1; i < seq.length - 1; i++) {
+// //   if (diff === 0) {
+// //     diff += seq[i] - seq[0];
+// //   }
+// //   if (seq[i + 1] - seq[i] != diff) {
+// //     result += seq[i] + diff;
+// //   }
+// // }
+
+// // console.log (result)
